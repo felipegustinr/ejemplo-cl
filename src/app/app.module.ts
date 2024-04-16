f02ce8d0271c5a7c453a27f92ece0df145721061
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2024-04-15 19:40:47 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PruebaComponent } from './components/prueba/prueba.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PruebaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
