446853039510e13de707af42c866729a785cdf5e
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2024-04-17 18:55:28 -0500

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  usuarios = [
+    {
+    id: "1",
+    usr: "Usuario 1",
+    edad: 20
+    },
+    {
+    id: "2",
+    usr: "Usuario 2",
+    edad: 21
+    },
+    {
+    id: "3",
+    usr: "Usuario 3",
+    edad: 22
+    },
+    {
+    id: "4",
+    usr: "Usuario 4",
+    edad: 23
+    },
+    {
+    id: "5",
+    usr: "Usuario 5",
+    edad: 24
+    },
+    ];
+    constructor() { }
+    ngOnInit() {
+    }
+   
 }
