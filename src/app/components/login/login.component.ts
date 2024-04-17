fb5b663f896bb0074a3327474450a255dd1f6b98
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2024-04-17 18:50:39 -0500

    Update Login.component.ts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,29 +1,24 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   is_error = false;
   user = {
-  usr: "",
-  pass: ""
+    usr: '',
+    pass: '',
   };
-  constructor(
-  private router: Router,
-  ) { }
-  ngOnInit() {
-  }
+  constructor(private router: Router) {}
+  ngOnInit() {}
   validarUsuario() {
-  if (this.user.usr == 'admin' && this.user.pass == '123') {
-  this.is_error = false;
-  this.router.navigate(['/home']);
+    if (this.user.usr == 'admin' && this.user.pass == '123') {
+      this.is_error = false;
+      this.router.navigate(['/home']);
+    } else {
+      this.is_error = true;
+    }
   }
-  else {
-  this.is_error = true;
-  }
-  }
- 
 }
