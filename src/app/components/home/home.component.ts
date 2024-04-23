74d49f806145ab58178ea94cde52c809b60d9a75
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2024-04-22 19:52:28 -0500

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,33 +8,8 @@ import { ProjectService } from '../../services/project/project.service';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
-  usuarios = [
-    {
-      id: '1',
-      usr: 'Usuario 1',
-      edad: 20,
-    },
-    {
-      id: '2',
-      usr: 'Usuario 2',
-      edad: 21,
-    },
-    {
-      id: '3',
-      usr: 'Usuario 3',
-      edad: 22,
-    },
-    {
-      id: '4',
-      usr: 'Usuario 4',
-      edad: 23,
-    },
-    {
-      id: '5',
-      usr: 'Usuario 5',
-      edad: 24,
-    },
-  ];
-  constructor() {}
-  ngOnInit() {}
+  constructor(private router: Router, private projectService: ProjectService) {}
+  ngOnInit(
+    
+  ) {}
 }
